8d6788ab6d494293ab47b0241ff83ab9d295062c
Flacomanso <aballayesteban2@gmail.com>
2024-08-13 21:07:41 -0300

    git commit -m "Fixed bug in login feature"

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   root "pages#welcome"
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-
-  # Defines the root path route ("/")
-  # root "posts#index"
+  namespace :admin do
+    get 'dashboard', to: 'dashboard#index'
+  end
 end
+
